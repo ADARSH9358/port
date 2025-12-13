263ea55eac86fd9a41767f8b41e1a06b08ffee87
Adarsh kumar <122091788+ADARSH9358@users.noreply.github.com>
2025-12-13 16:57:53 +0530

    x

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,70 +1,100 @@
-import { BrowserRouter } from "react-router-dom"
-import { Container } from "./styles"
-import ScrollAnimation from "react-animate-on-scroll"
-import Illustration from "../../assets/illustration.svg"
-import { NavHashLink } from "react-router-hash-link"
-import linkedin from '../../assets/linkedin.svg'
-import githubIcon from '../../assets/github.svg'
-import whatsapp from '../../assets/whatsapp.svg'
-import Hello from '../../assets/Hello.gif'
-import telegram from '../../assets/telegram.svg'
+import { useEffect } from "react";
+import { BrowserRouter } from "react-router-dom";
+import { NavHashLink } from "react-router-hash-link";
+
+import { Container } from "./styles";
+
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+import Illustration from "../../assets/illustration.svg";
+import linkedin from "../../assets/linkedin.svg";
+import githubIcon from "../../assets/github.svg";
+import whatsapp from "../../assets/whatsapp.svg";
+import telegram from "../../assets/telegram.svg";
+import Hello from "../../assets/Hello.gif";
+
 export function Hero() {
+
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,
+      once: true,
+      easing: "ease-in-out",
+    });
+  }, []);
+
   return (
     <Container id="home">
       <div className="hero-text">
-        <ScrollAnimation animateIn="fadeInUp">
-          <p>Hello <img src={Hello} alt="Hello" width="20px"/>, I'm</p>
-        </ScrollAnimation>
-        <ScrollAnimation animateIn="fadeInUp" delay={0.2 * 1000}>
-          <h1>Vinayak Singh</h1>
-        </ScrollAnimation>
-        <ScrollAnimation animateIn="fadeInUp" delay={0.4 * 1000}>
-          <h3>Software Engineer</h3>
-        </ScrollAnimation>
-        <ScrollAnimation animateIn="fadeInUp" delay={0.6 * 1000}>
-          <p className="small-resume">1 Year Experience</p>
-        </ScrollAnimation>
-        <ScrollAnimation animateIn="fadeInUp" delay={0.8 * 1000}>
+
+        <p data-aos="fade-up">
+          Hello <img src={Hello} alt="Hello" width="20px" />, I'm
+        </p>
+
+        <h1 data-aos="fade-up" data-aos-delay="200">
+          Vinayak Singh
+        </h1>
+
+        <h3 data-aos="fade-up" data-aos-delay="400">
+          Software Engineer
+        </h3>
+
+        <p className="small-resume" data-aos="fade-up" data-aos-delay="600">
+          1 Year Experience
+        </p>
+
+        <div data-aos="fade-up" data-aos-delay="800">
           <BrowserRouter>
-            <NavHashLink smooth to="#contact" className="button">Contact</NavHashLink>
+            <NavHashLink smooth to="#contact" className="button">
+              Contact
+            </NavHashLink>
           </BrowserRouter>
-        </ScrollAnimation>
-        <ScrollAnimation animateIn="fadeInUp" delay={1 * 1000}>
-      <div className="social-media"><a
-        href="https://www.linkedin.com/in/codevinayak"
-        target="_blank"
-        rel="noreferrer"
-      >
-        <img src={linkedin} alt="Linkedin" />
-      </a>
-        <a
-          href="https://github.com/CodeVinayak/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img src={githubIcon} alt="GitHub" />
-        </a>
-        <a
-          href="https://api.whatsapp.com/send/?phone=%2B919630576848&text=Hello+Vinayak+I+found+your+contact+through+portfolio+site.%0A%0A"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img src={whatsapp} alt="Whatsapp" />
-        </a>
-        <a
-          href="https://t.me/CodeVinayak"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img src={telegram} alt="telegram" />
-        </a></div>
-        </ScrollAnimation>
+        </div>
+
+        <div className="social-media" data-aos="fade-up" data-aos-delay="1000">
+          <a
+            href="https://www.linkedin.com/in/codevinayak"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={linkedin} alt="LinkedIn" />
+          </a>
+
+          <a
+            href="https://github.com/CodeVinayak/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={githubIcon} alt="GitHub" />
+          </a>
+
+          <a
+            href="https://api.whatsapp.com/send/?phone=%2B919630576848&text=Hello+Vinayak+I+found+your+contact+through+portfolio+site.%0A%0A"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={whatsapp} alt="Whatsapp" />
+          </a>
+
+          <a
+            href="https://t.me/CodeVinayak"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={telegram} alt="Telegram" />
+          </a>
+        </div>
       </div>
+
       <div className="hero-image">
-        <ScrollAnimation animateIn="fadeInRight" delay={1 * 1000}>
-          <img src={Illustration} alt="Ilustração" />
-        </ScrollAnimation>
+        <img
+          src={Illustration}
+          alt="Illustration"
+          data-aos="fade-right"
+          data-aos-delay="1000"
+        />
       </div>
     </Container>
-  )
+  );
 }
